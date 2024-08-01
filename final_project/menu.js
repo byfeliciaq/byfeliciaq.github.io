3c982fe57cd7f740e1dd49254d72a5c83070bebb
byfeliciaq <missquerrey@gmail.com>
2024-08-01 16:57:58 -0400

    final project

---
+++ final_project/menu.js
@@ -0,0 +1,8 @@
+// menu.js
+function showmenu() {
+  document.getElementById('menuoverlay').style.display = 'block';
+}
+
+function hidemenu() {
+  document.getElementById('menuoverlay').style.display = 'none';
+}
